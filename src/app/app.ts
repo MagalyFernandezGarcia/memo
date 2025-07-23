c8c551e10527910eae048adff22066fd2d270871
Magaly <fernandezgarciamagaly@gmail.com>
2025-07-23 11:53:49 +0200

    creating reusable component for both toDo and done List

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToDoComponent } from './toDo/ToDo';
+
 import { AddTaskForm } from './addTaskForm/AddTaskForm';
 import { CommonModule } from '@angular/common';
+import { TaskList } from './taskList/Tasklist';
+import { ToDo, ToDoList } from './to-do-list';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToDoComponent, AddTaskForm, CommonModule],
+  imports: [RouterOutlet, AddTaskForm, CommonModule, TaskList],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('memo');
+  toDoList: ToDo[] = [];
+
+  constructor(private toDoListService: ToDoList) {
+    this.toDoList = toDoListService.toDoList;
+  }
+
   isAddFormOpen = false;
 }
--- src/app/taskList/Tasklist.ts
+++ src/app/taskList/Tasklist.ts
@@ -1,24 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { ToDo } from '../to-do-list';
 import { ToDoList } from '../to-do-list';
 import { CommonModule } from '@angular/common';
+import { I } from '@angular/cdk/keycodes';
 
 @Component({
+  selector: 'app-taskList',
+  templateUrl: './taskList.html',
+  styleUrls: ['./taskList.css'],
   standalone: true,
-  selector: 'app-toDo',
-  templateUrl: './toDo.html',
-  styleUrls: ['./toDo.css'],
   imports: [MatCardModule, MatCheckboxModule, MatButtonModule, CommonModule],
 })
-export class ToDoComponent {
-  toDoList: ToDo[] = [];
-
-  constructor(private toDoListService: ToDoList) {
-    this.toDoList = toDoListService.toDoList;
-  }
+export class TaskList {
+  @Input() toDoList: ToDo[] = [];
   @Output() openAddForm = new EventEmitter<void>();
 
   handleClick() {
